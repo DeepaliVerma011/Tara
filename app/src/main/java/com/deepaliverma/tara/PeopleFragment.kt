18f88d9f32b1bfcfdcbde8df4d8f1c4048d41e20
DeepaliVerma011 <ddeepaliverma2000@gmail.com>
2021-07-14 14:36:24 +0530

    Current User Invisible

--- app/src/main/java/com/deepaliverma/tara/PeopleFragment.kt
+++ app/src/main/java/com/deepaliverma/tara/PeopleFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.paging.PagedList
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.firebase.ui.firestore.paging.FirestorePagingAdapter
 import com.firebase.ui.firestore.paging.FirestorePagingOptions
 import com.firebase.ui.firestore.paging.LoadingState
@@ -16,16 +17,20 @@ import com.google.firebase.firestore.Query
 import kotlinx.android.synthetic.main.fragment_people.*
 import java.lang.Exception
 
+private const val DELETED_VIEW_TYPE = 1
+private const val NORMAL_VIEW_TYPE = 2
+
 class PeopleFragment : Fragment() {
 
-lateinit var mAdapter: FirestorePagingAdapter<User,UsersViewHolder>
-val auth by lazy{
-    FirebaseAuth.getInstance()
-}
-    val database by lazy{
-        FirebaseFirestore.getInstance().collection("users")
-            .orderBy("name",Query.Direction.DESCENDING)
+    lateinit var mAdapter: FirestorePagingAdapter<User, RecyclerView.ViewHolder>
+    val auth by lazy {
+        FirebaseAuth.getInstance()
     }
+    val database by lazy {
+        FirebaseFirestore.getInstance().collection("users")
+            .orderBy("name", Query.Direction.ASCENDING)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -36,50 +41,71 @@ val auth by lazy{
     }
 
     private fun setUpAdapter() {
-        val config= PagedList.Config.Builder()
+        val config = PagedList.Config.Builder()
             .setEnablePlaceholders(false)     //empty values before loading
             .setPageSize(10)                   //how many items in one call
             .setPrefetchDistance(2)           //no of pages required to be loaded initially
             .build()
 
-        val options= FirestorePagingOptions.Builder<User>()
+        val options = FirestorePagingOptions.Builder<User>()
             .setLifecycleOwner(viewLifecycleOwner)   //livedata
-            .setQuery(database,config,User::class.java)
+            .setQuery(database, config, User::class.java)
             .build()
-mAdapter=object :FirestorePagingAdapter<User,UsersViewHolder>(options){
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UsersViewHolder {
-        val view=layoutInflater.inflate(R.layout.list_item,parent,false)
-        return UsersViewHolder(view)
-    }
+        mAdapter = object : FirestorePagingAdapter<User, RecyclerView.ViewHolder>(options) {
+            override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+               return when (viewType) {
+                   NORMAL_VIEW_TYPE->UsersViewHolder(layoutInflater.inflate(R.layout.list_item,parent,false))
+                   else->EmptyViewHolder(layoutInflater.inflate(R.layout.empty_view,parent,false))
 
-    override fun onBindViewHolder(holder: UsersViewHolder, position: Int, model: User) {
-        holder.bind(user=model)
-    }
-
-    override fun onLoadingStateChanged(state: LoadingState) {
-        super.onLoadingStateChanged(state)
-        when(state){
-            LoadingState.ERROR->{
-
+               }
             }
-          LoadingState.FINISHED-> {}
-            LoadingState.LOADING_MORE-> {}
-            LoadingState.LOADING_INITIAL-> {}
-            LoadingState.LOADED-> {}
-        }
-    }
 
-    override fun onError(e: Exception) {
-        super.onError(e)
-    }
-}
+            override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int, model: User) {
+              if(holder is UsersViewHolder){
+                  holder.bind(user =model)
+              }
+                else{
+                    //TODO SOMETHING
+                }
+            }
+
+            override fun onLoadingStateChanged(state: LoadingState) {
+                super.onLoadingStateChanged(state)
+                when (state) {
+                    LoadingState.ERROR -> {
+
+                    }
+                    LoadingState.FINISHED -> {
+                    }
+                    LoadingState.LOADING_MORE -> {
+                    }
+                    LoadingState.LOADING_INITIAL -> {
+                    }
+                    LoadingState.LOADED -> {
+                    }
+                }
+            }
+
+            override fun onError(e: Exception) {
+                super.onError(e)
+            }
+
+            override fun getItemViewType(position: Int): Int {
+                val item = getItem(position)?.toObject(User::class.java)
+                return if (auth.uid == item!!.uid) {
+                    DELETED_VIEW_TYPE
+                } else {
+                    NORMAL_VIEW_TYPE
+                }
+            }
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        recyclerView.apply{
-            layoutManager=LinearLayoutManager(requireContext())
-            adapter=mAdapter
+        recyclerView.apply {
+            layoutManager = LinearLayoutManager(requireContext())
+            adapter = mAdapter
         }
     }
 }
